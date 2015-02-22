e9b95a8368984be3d2869f2ff35f2904401e1e7d
gr1zzly-be4r <jake.bruemmer@gmail.com>
2015-02-21 22:27:26 -0600

    First commit after starting work on Minesweeper game

---
+++ labs/objects/game_tile.rb
@@ -0,0 +1,41 @@
+#! usr/bin/env ruby
+
+# An object oriented take on the minesweeper game that's done in 131.
+# Since it is un-Rubylike to use 2d arrays, I'm going to try and 
+# make the game using a more object oriented approach.
+
+class GameTile
+
+  attr_accessor :adjacent, :adjacent_bombs
+
+  def initialize(up_left, up, up_right, left, right, down_left, down, down_right, is_bomb)
+    @adjacent = {
+      "up_left" => up_left,
+      "up" => up,
+      "up_right" => up_right,
+      "left" => left,
+      "right" => right,
+      "down_left" => down_left,
+      "down" => down,
+      "down_right" => down_right
+    }
+    @is_bomb = is_bomb
+    @adjacent_bombs = 0
+  end
+
+  # Method will iterate through the surrounding GameTiles to check how many bombs there are.
+  # The method returns the number of bombs that are adjacent to itself.
+  def find_adjacent_bombs
+    @adjacent.each do |key, value|
+      if value.is_bomb?
+        @adjacent_bombs += 1
+      end
+    end
+  end
+
+  def is_bomb?
+    @is_bomb
+  end
+
+end
+
---
+++ labs/objects/minesweeper.rb
@@ -0,0 +1,80 @@
+#! usr/bin/env ruby
+require_relative 'game_tile'
+
+puts "Welcome to the game of minesweeper!"
+puts "-----------------------------------"
+puts "What difficulty would you like to play with? 1 for expert, 2 for hard, 3 for medium, and 4 for easy"
+difficulty = gets.chomp.to_i
+
+# Create the board
+board = {}
+4.times do |row|
+  4.times do |column|
+    if rand < 0.25
+      board["(#{row}, #{column})"] = GameTile.new(nil, nil, nil, nil, nil, nil, nil, nil, true)
+    else
+      board["(#{row}, #{column})"] = GameTile.new(nil, nil, nil, nil, nil, nil, nil, nil, false)
+    end
+
+
+    # Set the left pointers to GameTile objects that have already been created.
+    if column > 0
+      board["(#{row}, #{column})"].adjacent["left"] = board["(#{row}, #{column - 1})"]
+    end
+   
+    # Set the up_left, up, and up_right pointers to objects that have been created. Edge 
+    # cases are automatically handled because Hashes return nil for keys that do not 
+    # exist in the hash.
+    if row > 0
+      board["(#{row}, #{column})"].adjacent["up"] = board["(#{row - 1}, #{column})"]
+      board["(#{row}, #{column})"].adjacent["up_left"] = board["(#{row - 1}, #{column - 1})"]
+      board["(#{row}, #{column})"].adjacent["up_right"] = board["(#{row - 1}, #{column + 1})"]
+    end
+    
+  end
+end
+
+4.times do |row|
+  4.times do |column|
+
+    # Set the right pointer object based on objects that have already been created.
+    if column < 3
+      board["(#{row}, #{column})"].adjacent["right"] = board["(#{row}, #{column + 1})"]
+    end
+
+    if row < 3
+      board["(#{row}, #{column})"].adjacent["down_left"] = board["(#{row + 1}, #{column - 1})"]
+      board["(#{row}, #{column})"].adjacent["down"] = board["(#{row + 1}, #{column})"]
+      board["(#{row}, #{column})"].adjacent["down_right"] = board["(#{row + 1}, #{column + 1})"]
+    end
+
+  end
+end
+
+# Search for bombs
+board.each do |key, value|
+  begin 
+    value.find_adjacent_bombs
+  rescue
+  end
+end
+
+4.times do |row|
+  4.times do |column|
+    print board["(#{row}, #{column})"].adjacent_bombs.to_s + " "
+  end
+  puts ''
+end
+
+4.times do |row|
+  4.times do |column|
+    if board["(#{row}, #{column})"].is_bomb?
+      print "* "
+    else 
+      print ". "
+    end
+  end
+  puts ''
+end
+
+
